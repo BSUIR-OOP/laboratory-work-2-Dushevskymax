f4dc67ebdf5b85e86809fbc9a45eb3f82315a085
Maxim Dushevsky <dushevskymax@mail.ru>
2022-04-05 03:11:33 +0300

    Lab2

---
+++ Figures/Segment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Segment : Figure
+    {
+        public override void Draw(Graphics g, Point click)
+        {
+            g.DrawLine(Pens.Black, click.X, click.Y, click.X+80, click.Y-80);
+        }
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            figures.Add(new Ellipse());
+            figures.Add(new Rectangle());
+            figures.Add(new Rhombus());
+            figures.Add(new Segment());
+            figures.Add(new Square());
+            figures.Add(new Triangle());
+        }
+
+        List<Figure> figures = new List<Figure>();
+
+        public Graphics g;
+        Point click;
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g = CreateGraphics();
+            g.Clear(Color.Azure);
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            click = e.Location;
+            figures[button_index].Draw(g, click);
+        }
+
+        int button_index;
+        private void button1_Click(object sender, EventArgs e)
+        {
+          button_index = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button_index = 1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button_index = 2;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+             button_index = 3;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button_index = 4;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            button_index = 5;
+        }
+    }
+}
